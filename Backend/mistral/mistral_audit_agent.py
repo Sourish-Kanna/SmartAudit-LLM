63781f35d4382e13ceacce9f1e3a70cf60a05fbd
Sourish Kanna <115524993+Sourish-Kanna@users.noreply.github.com>
2025-12-02 17:35:05 +0530

    Fix LangChain import errors and finalize file structure

--- Backend/mistral/mistral_audit_agent.py
+++ Backend/mistral/mistral_audit_agent.py
@@ -3,7 +3,7 @@ import json5
 import re
 from typing import List, Dict, Any
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from mistral.audit_logic import MistralAuditLogic
 
